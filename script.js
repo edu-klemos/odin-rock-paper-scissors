399eee332f27507f051591594e8ea442a1cccf3c
Eduardo <eduardo.klock@gmail.com>
2024-10-28 17:50:36 -0300

    Add function getComputerChoice
    It returns randomly if the the computer plays rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("oi");
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+
+        case 1:
+            return "paper";
+
+        case 2:
+            return "scissors";
+    }
+}
