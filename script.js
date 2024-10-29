6649e9e2642269b3a5930b0d7fbe176fc23dde22
Eduardo <eduardo.klock@gmail.com>
2024-10-28 22:36:14 -0300

    Add function to play a round of the game
    This function checks both the inputs from the player and the computer and check who wins, or if it was a draw.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -29,3 +33,52 @@ function getHumanChoice() {
 
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors.");
+            humanScore++;
+            return;
+        }
+
+        if (computerChoice === "paper") {
+            console.log("You loose! Paper beats Rock.");
+            computerScore++;
+            return;
+        }
+        
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock.");
+            humanScore++;
+            return;
+        }
+        if (computerChoice === "scissors") {
+            console.log("You loose! Scissors beats Paper");
+            computerScore++;
+            return;
+        }
+    }
+
+    if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+            return;
+        }
+        if (computerChoice === "rock") {
+            console.log("You loose! Rock beats Scissors");
+            computerScore++;
+            return;
+        }
+    }
+    console.log("Draw Round!")
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
